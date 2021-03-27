299e67cb1c21efbb712bd8fb7ac9889b7867d23b
Fraser Watson <fjw3@hw.ac.uk>
2021-03-27 23:37:21 +0000

    Wrote tests for exclusion

--- test/F28DA_CW2/FlyPlannerTest.java
+++ test/F28DA_CW2/FlyPlannerTest.java
@@ -1,8 +1,10 @@
 package F28DA_CW2;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -26,10 +28,34 @@ public class FlyPlannerTest {
 	// You can get inspiration from the tests in FlyPlannerProvidedTest
 	// that uses the provided data set but also from the
 	// leastCostCustomTest test that uses a custom-made graph
-
+	
 	@Test
-	public void anotherTest() {
-		fail("To be implemented");
+	public void testExclusion() throws FlyPlannerException {
+		// Check exclusions work when there is a path that does not require excluded airports
+		List<String> exclude = new ArrayList<>();
+		exclude.add("AMS");
+		TripImpl trip = fi.leastCost("NCL", "NTL", exclude);
+		List<String> flights = trip.getFlights();
+		assertEquals(5, trip.getFlights().size());
+		
+		assertEquals("BA2600", flights.get(0));
+		assertEquals("BA9502", flights.get(1));
+		assertEquals("LH6123", flights.get(2));
+		assertEquals("CX7100", flights.get(3));
+		assertEquals("QF0640", flights.get(4));
+		
+	}
+	
+	@Test
+	public void testExclusionNotPermanent() throws FlyPlannerException {
+		// Checks that excluded airports are not permanent for the lifetime of the FlyPlannerImpl
+		List<String> exclude = new ArrayList<>();
+		exclude.add("AMS");
+		fi.leastCost("NCL", "NTL", exclude);
+		
+		TripImpl trip = fi.leastCost("NCL", "NTL");
+		assertEquals(4, trip.getFlights().size());
+		
 	}
 
 }
